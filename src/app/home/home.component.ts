cd6918414547961e7edd06c3af5659645abb628e
RyanMontville <ryan.a.montville@gmail.com>
2025-05-12 10:18:31 -0400

    deploying latest build

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive, ActivatedRoute } from '@angular/router';
 import { Link, Project } from '../project.model';
 import { projectsData } from '../data';
 import { ProjectOverviewComponent } from "../project-overview/project-overview.component";
 import { ProjectAccordionComponent } from "../project-accordion/project-accordion.component";
+import { TheService } from '../the.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, CommonModule, RouterOutlet, RouterLink, RouterLinkActive, ProjectOverviewComponent, ProjectAccordionComponent],
+  imports: [HeaderComponent, CommonModule, ProjectOverviewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   projectsToShow: Project[] = projectsData;
   projectsHeader: string = "All Projects";
   isArrayFiltered: boolean = false;
+
+  constructor(
+    private theService: TheService,
+    private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    let param = this.route.snapshot.queryParamMap.get('goto');
+    if (param) {
+      this.theService.goTo(param);
+    }
+  }
 
   filterProjects(value: any) {
     let skill = value.target.value; 
